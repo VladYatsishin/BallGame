99af821347215724fb526c2e5a1d9d08da7d1d45
VladYatsishin <sunnnnnshinnner445@gmail.com>
2023-12-06 22:07:29 +0200

    Initial commit

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speed = 5;
+    Rigidbody rb;
+    void Start()
+{
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        Vector3 vector3 = new Vector3(horizontalInput * speed, rb.velocity.y, verticalInput * speed);
+        rb.AddForce(vector3);
+    }
+}
